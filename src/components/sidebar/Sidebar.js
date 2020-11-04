ae700f2d9ebd04b914e223f6d2aad5aeb34d3664
oliver <olivercastroteran@gmail.com>
2020-11-04 13:25:14 -0400

    playlists render into sidebar & PRO button added

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,12 @@
 import './Sidebar.scss';
 import { ReactComponent as SpotifyIcon } from '../../assets/images/spotify.svg';
+import { ReactComponent as ProIcon } from '../../assets/images/disc.svg';
 import { ReactComponent as HomeIcon } from '../../assets/icons/home.svg';
 import { ReactComponent as FavoriteIcon } from '../../assets/icons/favorite.svg';
 import { ReactComponent as UserIcon } from '../../assets/icons/user.svg';
 import { Link, NavLink } from 'react-router-dom';
+import { playlistsData } from '../music/playlistsData';
+import Playlist from '../music/playlist/Playlist';
 
 const Sidebar = () => {
   return (
@@ -31,6 +34,20 @@ const Sidebar = () => {
           <span>User</span>
         </NavLink>
       </nav>
+
+      <div className="sidebar__playlists">
+        <h4>PLAYLISTS</h4>
+        {playlistsData.map((playlist) => (
+          <Playlist key={playlist.id} title={playlist.title} />
+        ))}
+      </div>
+
+      <div className="sidebar__pro">
+        <ProIcon />
+        <Link to="/pro" className="sidebar__pro--btn">
+          PRO
+        </Link>
+      </div>
     </div>
   );
 };
