a6cb245bf685f5e2fad4dd580b66df334e2388c0
oliver <olivercastroteran@gmail.com>
2020-11-11 13:12:57 -0400

    dashboard header UI builded

--- src/views/home/components/header/Header.js
+++ src/views/home/components/header/Header.js
@@ -1,9 +1,26 @@
 import './Header.scss';
+import { Link, useHistory } from 'react-router-dom';
+import { ReactComponent as GoBackIcon } from '../../../../assets/icons/go-back.svg';
+import { ReactComponent as UserIcon } from '../../../../assets/icons/user.svg';
 
 const Header = () => {
+  const history = useHistory();
+
   return (
     <div className="header">
-      <h1>Header</h1>
+      <div className="header__go-back" onClick={() => history.goBack()}>
+        <GoBackIcon />
+      </div>
+
+      <div className="header__search">
+        <input type="text" />
+        <button className="header__search--btn">Search</button>
+      </div>
+
+      <Link to="/user" className="header__user">
+        <UserIcon />
+        <span>Oliver CT</span>
+      </Link>
     </div>
   );
 };
