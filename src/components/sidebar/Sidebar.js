d4de00eb32f5b20923b06cf0058e440f6b58d297
oliver <olivercastroteran@gmail.com>
2020-11-22 12:03:32 -0400

    responsive-design || monitors & laptops

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -11,14 +11,16 @@ import { setPlaylists } from '../../store/actions/musicActions';
 import { openInfo } from '../../store/actions/infoActions';
 
 const Sidebar = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const { docs } = useFirestore('playlists');
   const playlists = useSelector((state) => state.music.playlists);
-  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    setIsLoading((prevIsLoading) => !prevIsLoading);
     dispatch(setPlaylists(docs));
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
   }, [docs, dispatch]);
 
   return (
@@ -51,16 +53,16 @@ const Sidebar = () => {
 
       <div className="sidebar__playlists">
         <h4>PLAYLISTS</h4>
-        {!isLoading && <Spinner />}
+        {isLoading && <Spinner />}
         {playlists?.map((playlist) => (
           <Playlist key={playlist.id} title={playlist.title} />
         ))}
       </div>
 
-      <div className="sidebar__pro">
+      <div className="sidebar__info">
         <ProIcon />
         <button
-          className="sidebar__pro--btn"
+          className="sidebar__info--btn"
           onClick={() => dispatch(openInfo())}
         >
           APP INFO
